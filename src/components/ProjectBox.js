734ad7749b3cfee3cd07b7a214fec80453bd4081
Felix Fernando <felixzhuang1@gmail.com>
2022-06-06 14:27:40 +0700

    small code refactoring

--- src/components/ProjectBox.js
+++ src/components/ProjectBox.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react'
 import './ProjectBox.css'
-import {SiReact, SiFigma, SiJavascript, SiFlutter, SiFirebase, SiHtml5, SiCss3} from 'react-icons/si'
+import {SiReact, SiFigma, SiJavascript, SiFlutter, SiFirebase, SiHtml5, SiCss3, SiStrapi} from 'react-icons/si'
 import { ModalContext } from '../context/global'
 
 function ProjectBox(props) {
@@ -20,21 +20,14 @@ function ProjectBox(props) {
                     <h3>{props.type}</h3>
                     <div className='stacks'>
                         {stacks.map((stack,key) => {
-                            if(stack == "react") {
-                                return (<SiReact className='stack-icon'></SiReact>)
-                            } else if(stack == "figma") {
-                                return (<SiFigma className='stack-icon'></SiFigma>)
-                            } else if(stack == "flutter") {
-                                return (<SiFlutter className='stack-icon'></SiFlutter>)
-                            } else if(stack == "firebase") {
-                                return (<SiFirebase className='stack-icon'></SiFirebase>)
-                            } else if(stack == "js") {
-                                return (<SiJavascript className='stack-icon'></SiJavascript>)
-                            } else if(stack == "html") {
-                                return (<SiHtml5 className='stack-icon'></SiHtml5>)
-                            } else if(stack == "css") {
-                                return (<SiCss3 className='stack-icon'></SiCss3>)
-                            } 
+                            if(stack == "react") return (<SiReact className='stack-icon'></SiReact>)
+                            else if(stack == "figma") return (<SiFigma className='stack-icon'></SiFigma>)
+                            else if(stack == "flutter") return (<SiFlutter className='stack-icon'></SiFlutter>)
+                            else if(stack == "firebase") return (<SiFirebase className='stack-icon'></SiFirebase>)
+                            else if(stack == "js") return (<SiJavascript className='stack-icon'></SiJavascript>)
+                            else if(stack == "html") return (<SiHtml5 className='stack-icon'></SiHtml5>)
+                            else if(stack == "css") return (<SiCss3 className='stack-icon'></SiCss3>)
+                            else if(stack == "strapi") return (<SiStrapi className='stack-icon'></SiStrapi>)
                         })
                         }
                     </div>
