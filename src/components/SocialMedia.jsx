b24a2162cebefe3094ca8fc81e5377f8be398255
Felix Fernando <felixzhuang1@gmail.com>
2022-07-13 17:13:55 +0700

    added youtube link

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './SocialMedia.css'
 
-import {FaLinkedin, FaInstagram, FaGithub, FaTwitter} from 'react-icons/fa'
+import {FaLinkedin, FaInstagram, FaGithub, FaTwitter, FaYoutube } from 'react-icons/fa'
 
 function SocialMedia() {
     return (
@@ -11,6 +11,7 @@ function SocialMedia() {
                 <a href='https://github.com/FelixFern'><FaGithub></FaGithub></a>
                 <a href='https://www.linkedin.com/in/felix-fern/'><FaLinkedin></FaLinkedin></a>
                 <a href='https://https://www.instagram.com/felixfern_/'><FaInstagram></FaInstagram></a>
+                <a href='https://www.youtube.com/channel/UCK-KH0huKdyiDQUvQZQyy_A'><FaYoutube></FaYoutube></a>
             </div>
         </div>
     )
